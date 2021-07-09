39c96bb12bc7153e11e3f0b5e7a6ebc29ad9c629
Carlos <carlosaugustocf99@gmail.com>
2021-07-09 15:15:51 -0300

    service upload

--- src/app/components/products/product-api/upload.service.ts
+++ src/app/components/products/product-api/upload.service.ts
@@ -1,6 +1,11 @@
-import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { FilesType } from './../product-img/files.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EMPTY, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +14,27 @@ export class UploadService {
 
   private baseUrl = 'http://localhost:8080';
 
-  constructor(private http: HttpClient) { }
 
+  constructor( private http: HttpClient, private snackbar: MatSnackBar){}
+
+  showMessage(msg: string): void{
+    this.snackbar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+  // return all list files
+  getAllFiles(): Observable<FilesType[]> {
+    return this.http.get<FilesType[]>(`${this.baseUrl}/test`);
+  }
+
+  // file per id
+  getPerId(id: string): Observable<FilesType> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  // define function to upload files
   upload(file: File): Observable<HttpEvent<any>> {
     const formData: FormData = new FormData();
 
@@ -24,8 +48,15 @@ export class UploadService {
     return this.http.request(req);
   }
 
-  getFiles(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/files`);
+  deleteFileById(id: string): Observable<FilesType> {
+    const url = `${this.baseUrl}/test/${id}`;
+    console.log(url);
+    return this.http.delete<FilesType>(url).pipe(
+      catchError(e => this.errorHandler(e))
+    );
   }
-
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu um error!');
+    return EMPTY;
+  }
 }
